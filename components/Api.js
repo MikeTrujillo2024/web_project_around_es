a740795182150968be0a9b4a20418b3282ac4afd
MikeTrujillo2024 <trujillomike121986@gmail.com>
2026-03-11 15:44:28 -0600

    agregamos una clase que controle las peticiones de la api

---
+++ components/Api.js
@@ -0,0 +1,11 @@
+/**
+ * esta clase controla todas las solicitudes que llegan por index
+ */
+export class Api {
+    constructor(baseUrl, headers) {
+        this._baseUrl = baseUrl;
+        this._headers = headers
+    }
+
+    
+}
